50a28523813f8ded20d5329cf00c46ddb4e17df9
Shadowsniper784 <>
2021-05-22 14:35:04 +0000

    added thing if none specified

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 app.get('/api', (req, res, next) => {
+	const a = req.query.a || 2;
 	const text = req.query.q;
-	let r = [];
-	const lext = text.toLowerCase();
-	const run = (docs, page) => {
+	if (text) run(a);
+	else no('q');
+	function no(what) {
+		return res.format({
+			'text/html': function() {
+				res.send(`<h>No ${what} specified</h>`);
+			}
+		});
+	}
+	function run(amount) {
+	  	let r = [];
+		const lext = text.toLowerCase();
+		var docs = require('./guide.json');
 		var result = docs.file.filter(function(item) {
 			const title = item.title.toLowerCase();
 			var e = item.children.find(it => {
@@ -13,14 +24,13 @@ app.get('/api', (req, res, next) => {
 			return title.includes(lext) || lext.includes(title) || e;
 		});
 		r.push(result);
-		return r;
-	};
-	r = run(require('./guide.json'), 1);
-	res.format({
-		'application/json': function() {
-			res.send(r);
-		}
-	});
+		r.length = amount;
+		res.format({
+			'application/json': function() {
+				res.send(r);
+			}
+		});
+	}
 });
 
 app.get('/', (req, res, next) => {
